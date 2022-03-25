c97121af66af921410997173bcabcf73aa84a932
Gregory Hawkins <91228189+gthawkins24@users.noreply.github.com>
2022-03-24 22:27:01 -0400

    thought controller functions finished

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -44,7 +44,78 @@ const thoughtController = {
     .catch(err => err.json(err));
   },
 
-  
+  updateThought({ params, body }, res) {
+    Thought.findOneAndUpdate(
+    { _id: params.thoughtId }, 
+    body, 
+    { new: true, runValidators })
+    .then(dbThoughtData => {
+      if (!dbThoughtData) {
+        res.status(404).json({ message: 'No thought found with this ID' });
+        return;
+      }
+      res.json(dbThoughtData);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+      console.log(err);
+    })
+  },
+
+  removeThought({ params, body }, res) {
+    Thought.findOneAndDelete({ _id: params.thoughtId })
+    .then(deletedThought => {
+      if (!deletedThought) {
+        res.status(404).json({ message: 'No thought found with this ID' });
+        return;
+      }
+      return User.findOneAndUpdate(
+        { _id: params.userId },
+        { $pull: { thoughts: params.thoughId } },
+        { new: true }
+      );
+    })
+    .then(dbUserData => {
+      if(!dbUserData) {
+        res.status(404).json({ message: 'No user found with this ID' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+      console.log(err);
+    })
+  },
+
+  addReaction({ params, body }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $push: { reactions: body } },
+      { new: true, runValidators: true }
+    )
+    .then(dbThoughtData => {
+      if (!dbThoughtData) {
+        res.status(404).json({ message: 'No thought found with this ID' });
+        return;
+      }
+      res.json(dbThoughtData);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+      console.log(err);
+    })
+  },
+
+  removeReaction({ params }, res) {
+    Thought.findOneAndUpdate(
+        { _id: params.thoughtId },
+        { $pull: { reactions: { reactionId: params.reactionId } } },
+        { runValidators: true, new: true }
+    )
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.json(err));
+  }
 }
 
 module.exports = thoughtController;
